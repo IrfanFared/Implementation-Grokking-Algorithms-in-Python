5d8332652fd85a479fafd293770fc9337e9022b8
Irfan <irfanpratamaallabout23@gmail.com>
2025-07-18 10:07:17 +0700

    Add function comprehension implementation

---
+++ hal baru yang dipelajari/funtion_conperhension.py
@@ -0,0 +1,12 @@
+# funtion coperhension adalahah sebuah fungsi yang digunakan untuk
+# mengembalikan sebuah nila yang dihasilkan dari sebuah iterble
+# degan mengunakan fungsi lamda
+# contoh fungsi comprehension
+def fungsi_comprehension(iterable):
+    return [x for x in iterable if x % 2 == 0]
+
+fungsi_comprehension([1, 2, 3, 4, 5, 6]) # Output: [2, 4, 6]  
+
+
+
+#biasanya digunakan untu mengembalika nilai yang dihasilkan dari sebuah iterable
